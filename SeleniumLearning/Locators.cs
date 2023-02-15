ceebdec2df843335af73799bb3abc005c1f20793
Rommer Chu <rommer.chu@gmail.com>
2023-02-15 17:02:16 -0500

    Selenium Support, Selenium Extras, WebDriverWait

--- SeleniumLearning/Locators.cs
+++ SeleniumLearning/Locators.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using WebDriverManager.DriverConfigs.Impl;
 using NUnit.Framework.Internal.Execution;
 using OpenQA.Selenium.DevTools.V107.SystemInfo;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.DevTools.V107.Input;
 
 namespace SeleniumLearning
 {
@@ -24,6 +26,10 @@ namespace SeleniumLearning
             new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
             driver = new ChromeDriver();
 
+            //Implicit wait 5 sec can be declared globally
+            // Better than Thread.Sleep(3000)
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
             //Point to the webform
             driver.Url = "https://rahulshettyacademy.com/loginpagePractise/";
 
@@ -31,9 +37,6 @@ namespace SeleniumLearning
             System.Drawing.Size windowSize = new System.Drawing.Size(1184, 1240);
 
             driver.Manage().Window.Size = windowSize;
-
-
-
 
         }
 
@@ -47,17 +50,32 @@ namespace SeleniumLearning
 
             driver.FindElement(By.Id("username")).SendKeys("rommer");
             driver.FindElement(By.Id("password")).SendKeys("test123");
+            //tagName[@attribute = 'value']
             driver.FindElement(By.CssSelector("input[value='admin']")).Click();
             driver.FindElement(By.CssSelector("option[value='teach']")).Click();
-            driver.FindElement(By.Id("terms")).Click();
 
-            //tagName[@attribute = 'value']
+            //driver.FindElement(By.Id("terms")).Click();  //Scenerio 1  ID
+            //driver.FindElement(By.XPath("//div[@class='form-group'][5]/label/span/input")).Click(); // Scenerio 2  Xpath
+            //driver.FindElement(By.XPath("//label[@class='text-info']/span/input")).Click(); // Scenerio 3 Xpath
+            driver.FindElement(By.CssSelector(".text-info span:nth-child(1) input")).Click(); // Scenerio 3  CssSelector
 
             //driver.FindElement(By.Id("signInBtn")).Click();
             driver.FindElement(By.XPath("//input[@value='Sign In']")).Click();
 
-            //DELAY: To allow the ALERT to pop-up
-            Thread.Sleep(3000);
+            //DELAY: To allow the ALERT to pop-up but unecessary
+            //Thread.Sleep(3000);
+
+
+            //Explicit wait implementation for specific element to trigger
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(8));
+
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+                .TextToBePresentInElement(driver.FindElement(By.Id("signInBtn")), "Sign In"));
+
+            //wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+            //    .ElementIsVisible(By.CssSelector("input[id=signInBtn]")));
+
+
 
             //Capture the ALERT MESSAGE after clicking Sign In button
             String errorMessage = driver.FindElement(By.ClassName("alert-danger")).Text;
@@ -75,11 +93,11 @@ namespace SeleniumLearning
             link.Click();
 
             //To match the URL and the href value
-            Assert.AreEqual(expectedUrl, hrefAttr);
 
-            
-            
-            //TestContext.Progress.WriteLine(hrefAttribute);
+            //Assert.AreEqual(expectedUrl, hrefAttr);  //Scenerio 1
+            //Assert.That(hrefAttr, Is.EqualTo(expectedUrl)); //Scenerio 2
+            Assert.That(hrefAttr.Equals(expectedUrl)); //Scenerio 3
+
 
             //driver.Close();
             //driver.Quit();
